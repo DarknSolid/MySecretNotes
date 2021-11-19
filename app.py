ff0c8e62dab98ff6935fe15e43d86a2d2988d7b7
tesp <tesp@itu.dk>
2021-11-19 10:06:17 +0100

    ?

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ INSERT INTO users VALUES(null,"admin", "password");
 INSERT INTO users VALUES(null,"bernardo", "omgMPC");
 INSERT INTO notes VALUES(null,2,"1993-09-23 10:10:10","hello my friend",1234567890);
 INSERT INTO notes VALUES(null,2,"1993-09-23 12:10:10","i want lunch pls",1234567891);
+INSERT INTO notes VALUES(null,1,"1993-09-23 12:10:10","Developer tool is fantastic (ctrl+shift+i)",1234567892);
 
 """)
 
